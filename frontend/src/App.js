0efe74f62a3c0702b34659fc834e6702a676789a
MatheusCEvangelista <matcastroea@gmail.com>
2025-11-15 15:08:53 -0300

    Feat: Login e Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/layout/Header";
-import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
+
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
 
 import TutoresList from "./pages/Tutores/TutoresList";
 import NovoTutor from "./pages/Tutores/NovoTutor";
@@ -17,41 +16,50 @@ import NovoMedico from "./pages/Medicos/NovoMedico";
 import AgendamentosList from "./pages/Agendamentos/AgendamentosList";
 import NovoAgendamento from "./pages/Agendamentos/NovoAgendamento";
 
-import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
+function AppLayout() {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === "/login" || location.pathname === "/register";
 
+  return (
+    <>
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/tutores" element={<TutoresList />} />
+        <Route path="/tutores/novo" element={<NovoTutor />} />
+
+        <Route path="/animais" element={<AnimaisList />} />
+        <Route path="/animais/novo" element={<NovoAnimal />} />
+
+        <Route path="/consultas" element={<ConsultasList />} />
+        <Route path="/consultas/nova" element={<NovaConsulta />} />
+
+        <Route path="/medicos" element={<MedicosList />} />
+        <Route path="/medicos/novo" element={<NovoMedico />} />
+
+        <Route path="/agendamentos" element={<AgendamentosList />} />
+        <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/vethub-ia" element={<div>Em desenvolvimento: VetHub + IA</div>} />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <div className="router-container"></div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/tutores" element={<TutoresList />} />
-          <Route path="/tutores/novo" element={<NovoTutor />} />
-
-          <Route path="/animais" element={<AnimaisList />} />
-          <Route path="/animais/novo" element={<NovoAnimal />} />
-
-          <Route path="/consultas" element={<ConsultasList />} />
-          <Route path="/consultas/nova" element={<NovaConsulta />} />
-
-          <Route path="/medicos" element={<MedicosList />} />
-          <Route path="/medicos/novo" element={<NovoMedico />} />
-
-          <Route path="/agendamentos" element={<AgendamentosList />} />
-          <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
-
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          <Route path="/vethub-ia" element={<div>Em desenvolvimento: VetHub + IA</div>} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <AppLayout />
+    </Router>
   );
 }
 
